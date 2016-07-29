d7683bb5b27d2e366ce0f9ae278516b050bf48dd
Eric Barbour <ebarbour@redhat.com>
2016-07-29 15:08:32 -0400

    Iterate notes and clicked event=remove note

--- src/app/containers/notes.ts
+++ src/app/containers/notes.ts
@@ -2,29 +2,45 @@ import { Component } from '@angular/core';
 import { NoteCard } from '../ui';
 
 @Component({
-    selector: 'notes-container',
-    directives: [NoteCard],
-    styles: [`
-.notes {
-  padding-top: 50px;
-}
-.creator {
-  margin-bottom: 40px;
-}
-`],
-    template: `
-<div class="row center-xs notes">
+  selector: 'notes-container',
+  directives: [
+    NoteCard
+  ],
+  styles: [`
+    .notes {
+      padding-top: 50px;
+    }
+    .creator {
+      margin-bottom: 40px;
+    }
+  `],
+  template: `
+    <div class="row center-xs notes">
       <div class="col-xs-6 creator">
-        Note Creator Here
+        note creator here
       </div>
       <div class="notes col-xs-8">
         <div class="row between-xs">
-          <note-card [note]="note"></note-card>
+          <note-card
+            class="col-xs-4"
+            [note]="note"
+            *ngFor="let note of notes; let i = index"
+            (checked)="onNoteChecked($event, i)"
+          >
+          </note-card>
         </div>
       </div>
     </div>
-`
+  `
 })
 export class Notes {
-    note = {title: 'new note', value: 'value of note', color: 'seagreen'}
+  notes = [
+    {title: 'Chores', value: 'Don\'t forget to clean up', color: 'lightblue'},
+    {title: 'Food', value: 'meal prep tonight please!', color: 'seagreen'},
+    {title: 'Shipping Number', value: '#234654hhd88', color: 'pink'}
+  ];
+
+  onNoteChecked(note, i) {
+    this.notes.splice(i, 1);
+  }
 }

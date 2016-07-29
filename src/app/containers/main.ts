6b3f697894ad0caffa351ec7837fc4283528c970
Eric Barbour <ebarbour@redhat.com>
2016-07-29 13:54:26 -0400

    Header UI component and init commit

---
+++ src/app/containers/main.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {AppBar} from '../ui'
+
+@Component({
+    selector: 'main-container',
+    directives: [AppBar],
+    template: `
+<div>
+  <app-bar></app-bar>
+  <main class="main">
+    content goes here
+  </main>
+</div>
+`
+})
+export class Main {}
